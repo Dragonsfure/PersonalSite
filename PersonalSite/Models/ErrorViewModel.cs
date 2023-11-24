9a6eab51b5c20bea1320243ddc61d9543577bc54
Dragonsfure <ae_a18@outlook.de>
2023-11-24 17:29:31 +0100

    First Commit

---
+++ PersonalSite/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace PersonalSite.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
