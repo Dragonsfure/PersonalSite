723fd1413b4383687a9576ef23f1b2895d50605c
Dragonsfure <ae_a18@outlook.de>
2023-11-28 17:58:27 +0100

    Added a DataContext
    - Added the DataContext that uses the Default ConnectionString with less Rights

---
+++ PersonalSite/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalSite.Models;
+
+namespace PersonalSite.Data {
+    public class DataContext : DbContext
+    {
+        public DataContext (DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SkillItem> SkillItem { get; set; } = default!;
+
+        public DbSet<BlogItem> BlogItems { get; set; } = default!;
+
+        public DbSet<MailItem> MailItems { get; set; } = default!;
+    }
+}
