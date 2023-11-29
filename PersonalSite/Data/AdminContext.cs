3cb75ae64296e1da49ce17a1e8173bbdb9f0b003
Dragonsfure <ae_a18@outlook.de>
2023-11-29 18:23:36 +0100

    Admincontext, Blogitem
    -Added the Blogitem used for the Datacontexts
    - Added an Admin Context (for use with Admin Connection String)
    - Changed the Favicon and webpicture
    - Added further files to gitignore

---
+++ PersonalSite/Data/AdminContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalSite.Models;
+
+namespace PersonalSite.Data {
+    public class AdminContext : DbContext
+    {
+        public AdminContext(DbContextOptions<AdminContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SkillItem> SkillItem { get; set; } = default!;
+
+        public DbSet<BlogItem> BlogItems { get; set; } = default!;
+
+        public DbSet<MailItem> MailItems { get; set; } = default!;
+    }
+}
---
+++ PersonalSite/Models/BlogItem.cs
@@ -0,0 +1,47 @@
+﻿namespace PersonalSite.Models {
+    public class BlogItem {
+
+        /// <summary>
+        /// Gets the Id or sets it, PrimaryKey for the Database.
+        /// </summary>
+        public int ID {
+            get; 
+            set;
+        }
+
+        /// <summary>
+        /// Gets the Title or sets it.
+        /// </summary>
+        public string Title {
+            get;
+            set;
+        } = string.Empty;
+
+        /// <summary>
+        /// Gets the ShortInfo or sets it.
+        /// </summary>
+        public string ShortInfo {
+            get;
+            set;
+        }= string.Empty;
+
+        /// <summary>
+        /// Gets the Text or sets it.
+        /// </summary>
+        public string Text {
+            get;
+            set;
+        }=string.Empty;
+
+        /// <summary>
+        /// Gets the Date or sets it.
+        /// </summary>
+        /// <remarks>
+        /// Gets updated, when Editing the Entry or Creating an new Entry.
+        /// </remarks>
+        public DateTime Date {
+            get;
+            set;
+        }= DateTime.Now;
+    }
+}
